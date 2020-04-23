112390d4e121e9a8275b954828047a37048763a9
ksb0511 <soobin796@gmail.com>
2020-04-23 19:03:42 +0900

    알고리즘

---
+++ ds1556/src/ds1556/Q10828_stack.java
@@ -0,0 +1,36 @@
+package ds1556;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Q10828_stack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		Stack<Integer> stack=new Stack<>();
+		
+		int n=scanner.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			String order = scanner.next();
+			if(order.contains("push")) {
+				int num = scanner.nextInt();
+				stack.push(num);
+			}
+			else if(order.equals("pop")) {
+				System.out.println(stack.isEmpty()?-1:stack.pop());
+			}
+			else if(order.equals("size"))
+				System.out.println(stack.size());	
+			else if(order.equals("top"))
+				System.out.println(stack.isEmpty()?-1:stack.peek());
+			else if(order.equals("empty")) {
+				System.out.println(stack.isEmpty()?1:0);
+			}
+		}
+
+	}
+
+}
