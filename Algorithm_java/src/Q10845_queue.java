539004876375bf43d935d3084393d6dd2d200fa8
ksb0511 <soobin796@gmail.com>
2020-04-26 20:41:46 +0900

    커밋좀 되라...

---
+++ Algorithm_java/src/Q10845_queue.java
@@ -0,0 +1,33 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q10845_queue {
+
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+	   int b=0;
+    Queue<Integer> q = new LinkedList<>();
+    
+    Scanner scanner = new Scanner(System.in);
+    int cnt = scanner.nextInt();
+    for(int i=0; i<cnt; i++) {
+    	String a = scanner.next();
+    	
+    	if(a.equals("push")) {
+    		b = scanner.nextInt();
+    		q.add(b);
+    	}else if(a.equals("front")){
+    		System.out.println(q.isEmpty()?-1:q.peek());
+    	}else if(a.equals("back")) {
+    		System.out.println(q.isEmpty()?-1:b);
+    	}else if(a.equals("size")) {
+    		System.out.println(q.size());
+    	}else if(a.equals("pop")) {
+    		System.out.println(q.isEmpty()?-1:q.poll());
+    	}else if(a.equals("empty")) {
+    		System.out.println(q.isEmpty()?1:0);
+    	}
+    }
+   }
+}
