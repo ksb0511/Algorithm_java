2ad00e0d657377dc624e1cdbca7ccf8fe718f78d
ksb0511 <soobin796@gmail.com>
2020-05-01 17:47:04 +0900

    dp3_finish

--- Algorithm_java/src/Q1463_dp1.java
+++ Algorithm_java/src/Q1463_dp1.java
@@ -18,6 +18,7 @@ public class Q1463_dp1 {
 			if(i%3==0) dp[i]=Math.min(dp[i/3]+1, dp[i]);
 			 //여기서 else-if를 쓰지 않는 이유는 2로도 나눠지고 3으로도 나눌 수 있는 숫자가 있을 수도 있기때문에 모든 경우의 수를 고려해야 함.
 		}
+		//
 		
 		System.out.println(dp[x]);
 	}
--- Algorithm_java/src/Q2156_dp3.java
+++ Algorithm_java/src/Q2156_dp3.java
@@ -1,47 +1,34 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
+ 
 public class Q2156_dp3 {
-	static int[] dp;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Scanner s = new Scanner(System.in);
-		
-		int x = s.nextInt();
-		dp=new int[x];
-		
-		for(int i=0;i<x;i++) {
-			dp[i]=s.nextInt();
-		}
-		
-		int sum = Math.max(one(x), two(x));
-		
-		System.out.println(sum);
-	}
-
-	public static Integer two(int x) {
-		int sum = 0; int limit=0;
-		for(int i=0;i<x;i++) { //처음부터 2개씩 선택
-			if(limit<2) {
-				sum +=dp[i];
-				limit++;
-			}
-			else limit=0;
-		}
-		return sum;
-	}
-	
-	public static Integer one(int x) {
-		int sum = 0; int limit=1;
-		for(int i=0;i<x;i++) { //처음부터 1개씩 선택
-			if(limit<2) {
-				sum +=dp[i];
-				limit++;
-			}
-			else limit=0;
-		}
-		return sum;
-		
-	}
+ 
+    public static void main(String[] args) {
+         
+        Scanner sc = new Scanner(System.in);
+         
+        int n = sc.nextInt();
+         
+        int p[] = new int[n+1];
+        int dp[] = new int[n+1];
+         
+        for(int i=1; i<=n; i++){
+            p[i] = sc.nextInt();
+        }
+        //점화식에서 i-3까지 가기때문에 1,2는 편의상 초기화 해준다.
+        dp[1] = p[1];
+        if(n > 1) // 1인 경우 터지기 때문에 예외처리
+        dp[2] = p[1] + p[2];        
+        for(int i=3; i<=n; i++){
+            //연속 0 번 마신경우   //연속 1번 마신경우 //연속 2번마신 경우
+            dp[i] = Math.max(dp[i-1],Math.max(dp[i-2]+p[i],dp[i-3]+p[i-1]+p[i]));
+ 
+        }
+         
+        System.out.println(dp[n]);
+         
+         
+ 
+    }
+ 
 }
