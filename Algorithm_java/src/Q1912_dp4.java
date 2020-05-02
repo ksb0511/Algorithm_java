6fe14b5af35739148f5316d914bd84c84e308c48
ksb0511 <soobin796@gmail.com>
2020-05-02 14:52:27 +0900

    연습했음

---
+++ Algorithm_java/src/Q1912_dp4.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Q1912_dp4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+	        Scanner sc = new Scanner(System.in);
+	        int n = sc.nextInt();
+	        int[] d = new int[100001];
+	        int[] a = new int[100001];
+	 
+	        for(int i=0; i<n; i++){
+	            a[i]=sc.nextInt();
+	            d[i]=a[i];
+	        }
+	    
+	        for(int i=1; i<n ; i++){       
+	           if(d[i-1]+a[i]>=d[i]){
+	                  d[i]=d[i-1]+a[i];
+	            }
+	        }
+	        
+	        int max=d[0];
+	        for(int i=1; i<n ; i++){
+	            if(d[i]>max)
+	                max=d[i];
+	        }
+	 
+	        System.out.println( max ); 
+	}
+
+}
