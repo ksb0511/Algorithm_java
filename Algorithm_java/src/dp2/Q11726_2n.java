670ad14fcfaab99dee883d649e1b3870bcac1e56
ksb0511 <soobin796@gmail.com>
2020-05-08 00:38:24 +0900

    2xn 타일링

--- Algorithm_java/src/dp2/Q11726_2n.java
+++ Algorithm_java/src/dp2/Q11726_2n.java
@@ -1,5 +1,24 @@
 package dp2;
 
+import java.util.*;
+
 public class Q11726_2n {
 
-}
+	static public void main(String args[]){
+		Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+   		int[] dp = new int[n+1];
+
+    	dp[0] = 1;
+    	dp[1] = 1;
+
+    	if(n>=2){
+        	for(int i = 2; i <= n; i++){
+            	dp[i] = (dp[i-1] + dp[i-2]);
+                dp[i] %= 10007;
+        	}
+    	}
+    	System.out.println(dp[n]);
+	}
+ }
